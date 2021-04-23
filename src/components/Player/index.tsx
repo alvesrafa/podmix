c1164804bdf1d5f6eb284261e10d3ead9481b652
Rafael Alves <alvesrafael17@hotmail.com>
2021-04-23 19:57:29 -0300

    finalizando contexto de audio, pause e play

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,10 +1,33 @@
+import Image from 'next/image';
 import { usePlayer } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
+import Slider from 'rc-slider';
+
+import 'rc-slider/assets/index.css';
+import { useEffect, useRef } from 'react';
 
 export function Player() {
-  const { currentEpisodeIndex, episodeList } = usePlayer();
+  const {
+    currentEpisodeIndex,
+    episodeList,
+    isPlaying,
+    togglePlay,
+    setPlayingState,
+  } = usePlayer();
+
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   const episode = episodeList[currentEpisodeIndex];
+
+  useEffect(() => {
+    if (!audioRef.current) return;
+
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
 
   return (
     <div className={styles.playerContainer}>
@@ -13,38 +36,74 @@ export function Player() {
         <strong>Tocando agora {episode?.title}</strong>
       </header>
 
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um podcast para ouvir</strong>
-      </div>
+      {episode ? (
+        <div className={styles.currentEpisode}>
+          <Image
+            width={592}
+            height={592}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um podcast para ouvir</strong>
+        </div>
+      )}
 
-      <footer className={styles.empty}>
+      <footer className={!episode ? styles.empty : ''}>
+        {episode && (
+          <audio
+            ref={audioRef}
+            src={episode.url}
+            autoPlay
+            onPlay={() => setPlayingState(true)}
+            onPause={() => setPlayingState(false)}
+          />
+        )}
+
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: '#04d361' }}
+                railStyle={{ backgroundColor: '#5E67C4' }}
+                handleStyle={{ borderWidth: 4, borderColor: '#5E67C4' }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
           </div>
           <span>00:00</span>
         </div>
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
 
           <button
             type="button"
             className={styles.playButton}
-            onClick={() => {}}
+            onClick={togglePlay}
+            disabled={!episode}
           >
-            <img src="/play.svg" alt="Tocar" />
+            {isPlaying ? (
+              <img src="/pause.svg" alt="Pausar" />
+            ) : (
+              <img src="/play.svg" alt="Tocar" />
+            )}
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </div>
--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -10,7 +10,10 @@ interface Episode {
 interface PlayerContextProps {
   episodeList: Episode[];
   currentEpisodeIndex: number;
+  isPlaying: boolean;
   play: (episode: any) => any;
+  togglePlay: () => any;
+  setPlayingState: (value: boolean) => any;
 }
 interface PlayerContextProviderProps {
   children: ReactNode;
@@ -21,14 +24,31 @@ const PlayerContext = createContext({} as PlayerContextProps);
 export function PlayerProvider({ children }: PlayerContextProviderProps) {
   const [episodeList, setEpisodeList] = useState<any>([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const play = (episode: any) => {
     setEpisodeList([episode]);
     setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  };
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  };
+  const setPlayingState = (state: boolean) => {
+    setIsPlaying(state);
   };
 
   return (
-    <PlayerContext.Provider value={{ currentEpisodeIndex, episodeList, play }}>
+    <PlayerContext.Provider
+      value={{
+        currentEpisodeIndex,
+        episodeList,
+        play,
+        isPlaying,
+        togglePlay,
+        setPlayingState,
+      }}
+    >
       {children}
     </PlayerContext.Provider>
   );
